13329012c4fa1c5276cafc7a6286e51702bd05a6
Vít Kutný <vitkutny@icloud.com>
2015-11-03 00:18:51 +0100

    Extension refactor

--- src/Blog/Extension.php
+++ src/Blog/Extension.php
@@ -7,32 +7,36 @@ use Ytnuk;
 
 final class Extension
 	extends Nette\DI\CompilerExtension
-	implements Ytnuk\Config\Provider
+	implements Ytnuk\Orm\Provider, Kdyby\Translation\DI\ITranslationProvider
 {
 
-	public function getConfigResources() : array
+	public function loadConfiguration()
+	{
+		parent::loadConfiguration();
+		$builder = $this->getContainerBuilder();
+		$builder->addDefinition($this->prefix('control'))->setImplement(Control\Factory::class);
+		$builder->addDefinition($this->prefix('category.control'))->setImplement(Category\Control\Factory::class);
+		$builder->addDefinition($this->prefix('category.form.control'))->setImplement(Category\Form\Control\Factory::class);
+		$builder->addDefinition($this->prefix('post.control'))->setImplement(Post\Control\Factory::class);
+		$builder->addDefinition($this->prefix('post.form.control'))->setImplement(Post\Form\Control\Factory::class);
+	}
+
+	public function getTranslationResources() : array
 	{
 		return [
-			Ytnuk\Orm\Extension::class => [
-				'repositories' => [
-					$this->prefix('categoryRepository') => Category\Repository::class,
-					$this->prefix('categoryDescriptionRepository') => Category\Description\Repository::class,
-					$this->prefix('postRepository') => Post\Repository::class,
-					$this->prefix('postDescriptionRepository') => Post\Description\Repository::class,
-					$this->prefix('postCategoryRepository') => Post\Category\Repository::class,
-				],
-			],
-			Kdyby\Translation\DI\TranslationExtension::class => [
-				'dirs' => [
-					__DIR__ . '/../../locale',
-				],
-			],
-			'services' => [
-				Control\Factory::class,
-				Category\Control\Factory::class,
-				Category\Form\Control\Factory::class,
-				Post\Control\Factory::class,
-				Post\Form\Control\Factory::class,
+			__DIR__ . '/../../locale',
+		];
+	}
+
+	public function getOrmResources() : array
+	{
+		return [
+			'repositories' => [
+				$this->prefix('categoryRepository') => Category\Repository::class,
+				$this->prefix('categoryDescriptionRepository') => Category\Description\Repository::class,
+				$this->prefix('postRepository') => Post\Repository::class,
+				$this->prefix('postDescriptionRepository') => Post\Description\Repository::class,
+				$this->prefix('postCategoryRepository') => Post\Category\Repository::class,
 			],
 		];
 	}
