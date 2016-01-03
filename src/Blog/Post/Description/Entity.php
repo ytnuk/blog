04814960408d04513de32db565dfbfbd2f69a468
Vít Kutný <vitkutny@icloud.com>
2016-01-03 03:45:16 +0100

    translation cascade remove

--- src/Blog/Post/Description/Entity.php
+++ src/Blog/Post/Description/Entity.php
@@ -7,7 +7,7 @@ use Ytnuk;
 /**
  * @property int $id {primary}
  * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Blog\Post\Entity $post {1:1 Ytnuk\Blog\Post\Entity::$description, isMain=true}
- * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Translation\Entity|NULL $value {1:1 Ytnuk\Translation\Entity, oneSided=true, isMain=true}
+ * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Translation\Entity|NULL $value {1:1 Ytnuk\Translation\Entity, oneSided=true, isMain=true, cascade=[persist, remove]}
  */
 final class Entity
 	extends Ytnuk\Orm\Entity
