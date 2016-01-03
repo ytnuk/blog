fb8df654710c552a936fce8bb17ff569884fbd19
Vít Kutný <vitkutny@icloud.com>
2016-01-03 05:56:11 +0100

    cascade remove, form relations init, translation container

--- src/Blog/Post/Entity.php
+++ src/Blog/Post/Entity.php
@@ -7,10 +7,10 @@ use Ytnuk;
 /**
  * @property int $id {primary}
  * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Translation\Entity $title {1:1 Ytnuk\Translation\Entity, oneSided=true, isMain=true, cascade=[persist, remove]}
- * @property Nextras\Orm\Relationships\OneHasOne|Description\Entity|NULL $description {1:1 Description\Entity::$post}
+ * @property Nextras\Orm\Relationships\OneHasOne|Description\Entity|NULL $description {1:1 Description\Entity::$post, cascade=[persist, remove]}
  * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Translation\Entity $content {1:1 Ytnuk\Translation\Entity, oneSided=true, isMain=true, cascade=[persist, remove]}
- * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Link\Entity $link {1:1 Ytnuk\Link\Entity, oneSided=true, isMain=true}
- * @property Nextras\Orm\Relationships\OneHasMany|Category\Entity[] $categoryNodes {1:m Category\Entity::$post}
+ * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Link\Entity $link {1:1 Ytnuk\Link\Entity, oneSided=true, isMain=true, cascade=[persist, remove]}
+ * @property Nextras\Orm\Relationships\OneHasMany|Category\Entity[] $categoryNodes {1:m Category\Entity::$post, cascade=[persist, remove]}
  * @property-read Ytnuk\Blog\Category\Entity|NULL $category {virtual}
  */
 final class Entity
